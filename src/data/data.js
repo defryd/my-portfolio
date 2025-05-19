6e39b6c102c46c57438e55ac9de0573a1f780855
‘Freddy <elipson39z@gmail.com>
2025-05-18 20:21:31 -0500

    refactor: separate data

---
+++ src/data/data.js
@@ -0,0 +1,79 @@
+
+const Ids = [ 'about', 'skills','experience', 'projects', 'education', 'certifications', 'contact' ];
+
+const projects = [
+    { title: "Project 1", tech: "Description of project 1", link: "https://github.com/defryd/catsApp.git", img: "/src/assets/cats.png" },
+    { title: "Project 2", tech: "Description of project 2", link: "#" },
+    { title: "Project 3", tech: "Description of project 3", link: "#" },
+    { title: "Project 4", tech: "Description of project 4", link: "#" },
+];
+
+const techStack = {
+    title: "Tecnologías",
+    items: [
+        { name: "React", url: "https://img.shields.io/badge/-React-45b8d8?style=flat-square&logo=react&logoColor=white" },
+        { name: "React Native", url: "https://img.shields.io/badge/-React Native-319ff0?style=flat-square&logo=react&logoColor=white" },
+        { name: "Redux", url: "https://img.shields.io/badge/-Redux-764ABC?style=flat-square&logo=redux&logoColor=white" },
+        { name: "Zustand", url: "https://img.shields.io/badge/-Zustand-752714?style=flat-square" },
+        { name: "NPM", url: "https://img.shields.io/badge/-NPM-CB3837?style=flat-square&logo=npm&logoColor=white" },
+        { name: "Tailwind CSS", url: "https://img.shields.io/badge/-TailwindCSS-06B6D4?style=flat-square&logo=tailwind-css&logoColor=white" },
+        { name: "GitHub Actions", url: "https://img.shields.io/badge/-Github_Actions-2088FF?style=flat-square&logo=github-actions&logoColor=white" },
+        { name: "Git", url: "https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white" },
+        { name: "Gitflow", url: "https://img.shields.io/badge/-Gitflow-F05032?style=flat-square&logo=git&logoColor=white" },
+        { name: "HTML5", url: "https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white" },
+        { name: "CSS", url: "https://img.shields.io/badge/-CSS-663399?style=flat-square&logo=css&logoColor=white" },
+        { name: "Bootstrap", url: "https://img.shields.io/badge/-Bootstrap-7952B3?style=flat-square&logo=bootstrap&logoColor=white" },
+        { name: "JavaScript", url: "https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black" },
+        { name: "TypeScript", url: "https://img.shields.io/badge/-TypeScript-007ACC?style=flat-square&logo=typescript&logoColor=white" },
+        { name: "Go", url: "https://img.shields.io/badge/-Go-00ADD8?style=flat-square&logo=go&logoColor=white" },
+        { name: "PHP", url: "https://img.shields.io/badge/-php-777BB4?style=flat-square&logo=php&logoColor=white" },
+        { name: "Java", url: "https://img.shields.io/badge/-Java-e91d1d?style=flat-square" },
+        { name: "Node.js", url: "https://img.shields.io/badge/-Nodejs-43853d?style=flat-square&logo=Node.js&logoColor=white" },
+        { name: "MongoDB", url: "https://img.shields.io/badge/-MongoDB-13aa52?style=flat-square&logo=mongodb&logoColor=white" },
+        { name: "SQLite", url: "https://img.shields.io/badge/-SQLite-003B57?style=flat-square&logo=sqlite&logoColor=white" },
+        { name: "MySQL", url: "https://img.shields.io/badge/-MySQL-4479A1?style=flat-square&logo=mysql&logoColor=white" },
+    ]
+}
+
+const experience = [
+    { company: "WIEDII SAS", role: "Analista de desarrollo", duration: "feb. 2024 - actualidad · 1 año 4 meses" },
+    { company: "Autónomo", role: "Freelance Programmer", duration: "feb. 2022 - ene. 2024 · 2 años" },
+    { company: "Alcaldía de Cúcuta · Contrato temporal", role: "Desarrollador de front-end", duration: "nov. 2021 - ene. 2022 · 3 meses" },
+];
+
+const education = [
+    { institution: "Universidad Francisco de Paula Santander", degree: "Grado en Ingeniería", duration: "ene. 2016 - dic. 2023" },
+    { institution: "Institution 2", degree: "Degree 2", duration: "Duration 2" },
+    { institution: "Institution 3", degree: "Degree 3", duration: "Duration 3" },
+];
+
+const certifications = [
+    { name: "React Native CLI: Aplicaciones nativas para IOS y Android", institution: "DevTalles", date: "feb. 2025", url: "https://cursos.devtalles.com/certificates/ogrxcs26gv", aptitudes: "React Native · TypeScript" },
+    { name: "Bootcamp Programación", institution: "Asoandes", date: "dic. 2024", url: "https://cursos.devtalles.com/certificates/ogrxcs26gv", aptitudes: "" },
+    { name: "TypeScript: Tu completa guía y manual de mano.", institution: "DevTalles", date: "sept. 2024", url: "https://cursos.devtalles.com/certificates/egwo9zrmga", aptitudes: "" },
+    { name: "Introduction to JavaScript", institution: "Sololearn", date: "ene. 2024", url: "https://www.sololearn.com/certificates/CC-7VO07T8K", aptitudes: "" },
+    { name: "Universidad JavaScript - De Cero a Experto JavaScript!", institution: "Udemy", date: "ene. 2024", url: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-0d4161ee-b3e3-4ddb-9f7c-bdf3b94c62cf.pdf", aptitudes: "" },
+    { name: "Curso de Google Hacking || Dorks Comandos para Hackear", institution: "Udemy", date: "dic. 2023", url: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-9eb69ff3-262a-4629-beb0-53de8da057f7.pdf", aptitudes: "" },
+
+    { name: "CCNA", institution: "Cisco", date: "Expedición sept. 2023 · Vencimiento: sept. 2026", url: "https://www.credly.com/badges/1f8a1f7c-6998-4adc-82bc-00a3def64b25/linked_in_profile", aptitudes: "" },
+    { name: "CCNA: Enterprise Networking, Security, and Automation", institution: "Cisco", date: "ago. 2023", url: "https://www.credly.com/badges/490be8d3-7288-41c4-8999-9aed41c2ab13/linked_in_profile", aptitudes: "" },
+    { name: "CCNA: Switching, Routing, and Wireless Essentials", institution: "Cisco", date: "jun. 2023", url: "https://www.credly.com/badges/57cc174c-83eb-478a-a6a3-f90b479b1b80/linked_in_profile", aptitudes: "" },
+    { name: "CCNA: Introduction to Networks", institution: "Cisco", date: "feb. 2023", url: "https://www.credly.com/badges/b1934a16-ca97-4969-971f-3066b6d71d0c/linked_in_profile", aptitudes: "" },
+
+    { name: "Curso de Responsive Design: Maquetación Mobile First", institution: "Platzi", date: "sept. 2022", url: "https://platzi.com/p/Defryd/curso/2030-mobile-first/diploma/detalle/", aptitudes: "" },
+    { name: "Curso Gratuito Ethical Hacking | Red Team", institution: "HACKERMENTOR CIBERSEGURIDAD S.A.S.", date: "jun. 2022", url: "", aptitudes: "" },
+    { name: "Curso de Inglés Básico A1: Tus Primeras Conversaciones", institution: "Platzi", date: "feb. 2022", url: "https://platzi.com/p/Defryd/curso/1945-ingles-principiantes/diploma/detalle/", aptitudes: "" },
+    { name: "Curso Definitivo de HTML y CSS", institution: "Platzi", date: "ene. 2022", url: "https://platzi.com/p/Defryd/curso/2008-html-css/diploma/detalle/", aptitudes: "" },
+    { name: "Curso Práctico de HTML y CSS", institution: "Platzi", date: "ene. 2022", url: "https://platzi.com/p/Defryd/curso/1758-html-practico/diploma/detalle/", aptitudes: "" },
+    { name: "Curso de Computación Básica", institution: "Platzi", date: "dic. 2021", url: "https://platzi.com/p/Defryd/curso/1741-computacion-basica-2019/diploma/detalle/", aptitudes: "" },
+    { name: "[Appsco] Curso profesional de Git y GitHub 2017", institution: "Platzi", date: "sept. 2020", url: "https://platzi.com/p/freddysebastianchiavera/curso/1194-git-appsco-2017/diploma/detalle/", aptitudes: "" },
+];
+
+export default {
+    Ids,
+    projects,
+    techStack,
+    experience,
+    education,
+    certifications
+}
